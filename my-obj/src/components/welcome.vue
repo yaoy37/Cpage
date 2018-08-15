34fb131a2d7def4107f449698a627d0a0ee6a80e
yaoyi <yaoyi@clerware.com>
2018-08-15 20:24:19 +0800

    '提交到本地'

---
+++ my-obj/src/components/welcome.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <span> {{msg}} </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'welcome',
+  data () {
+    return {
+      msg: '从前有群兽'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
